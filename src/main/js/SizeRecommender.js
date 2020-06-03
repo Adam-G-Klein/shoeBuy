ef840261240e1693e777825e870b6527acb942f9
elenarfowler <elenarfowler@gmail.com>
2020-06-03 01:39:54 -0700

    make request for size with error message if there are no connections found

--- src/main/js/SizeRecommender.js
+++ src/main/js/SizeRecommender.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import shoeImage from '../resources/static/exampleShoe.jpg';
 import ShoeStats from './ShoeStats';
+import restClient from './restClient.js';
 
 const HorizLineBottom = ({ color }) => (
     <hr
@@ -28,8 +29,21 @@ class SizeRecommender extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state ={
-			size: 10.5
+			size: null
 		}
+	}
+
+	componentDidMount() {
+		restClient({method: "GET",
+			path: "/api/sizeRecommend?model=" + this.props.location.shoeInfo.model + "&brand=" + this.props.location.shoeInfo.brand +
+				"&sex=" + this.props.location.shoeInfo.sex,
+            headers: {'Accept': 'application/json'}
+            }).done(response => {
+				console.log("response size: ", response.entity.response)
+				this.setState({
+					size: response.entity.response
+				})
+		});
 	}
 
 	render() {
@@ -37,77 +51,104 @@ class SizeRecommender extends React.Component {
 		let imageDisplay = [];
 		if(this.props.location.shoeInfo.imgURL === undefined || this.props.location.shoeInfo.imgURL == null){
 			imageDisplay.push(
-				<Card.Img variant="top" src={shoeImage} />
+				<div style={{padding: '20px', border: 'solid', borderColor: 'red'}}>
+					<Card.Img variant="top" src={shoeImage} />
+				</div>
 			)
 		}
 		else{
 			imageDisplay.push(
-				<Card.Img variant="top" src={this.props.location.shoeInfo.imgURL} />
+				<div style={{padding: '20px', border: 'solid', borderColor: 'red'}}>
+					<Card.Img variant="top" src={this.props.location.shoeInfo.imgURL} />
+				</div>
 			)
 		}
-		return (
-		    <div>
-			<div style={{margin: '40px'}} />
-			<Container>
-				<Row>
-				<Col lg={6} style={{textAlign: 'center'}}>
-					<h1 style={{color: 'white', fontSize: '80px'}}>{"TrueSize: " + (this.state.size).toString()}</h1>
-					<div style={{margin: '20px'}} />
-					<h2 style={{color: 'white', fontSize: '40px'}}>{this.props.location.shoeInfo.brand + " " + this.props.location.shoeInfo.model}</h2>
+
+		console.log("state size: ", this.state.size)
+		if(this.state.size === null || this.state.size === undefined){
+			return null
+		}
+		else if(this.state.size === "Shoe_Not_Found"){
+			return (
+				<div>
+          <div style={{margin: '40px'}}/>
+          <Container>
+          <Row>
+			  
+            <Col>
+			<h3 style={{color: 'white'}}>Sorry, we can't seem to find any connection from your profile to the shoe you're looking at. Try adding more shoes to your account.</h3>
+            </Col>
+			
+          </Row>
+          </Container>
+        </div>
+			)
+		}
+		else{
+			return (
+				<div>
+				<div style={{margin: '40px'}} />
+				<Container>
+					<Row>
+					<Col lg={6} style={{textAlign: 'center'}}>
+						<h1 style={{color: 'white', fontSize: '80px'}}>{"TrueSize: " + (this.state.size).toString()}</h1>
+						<div style={{margin: '20px'}} />
+						<h2 style={{color: 'white', fontSize: '40px'}}>{this.props.location.shoeInfo.brand + " " + this.props.location.shoeInfo.model}</h2>
+						<div style={{margin: '40px'}} />
+						<Row>
+							<Col style={{position: 'relative', left: 0, right: 0, top: 0, bottom: 0}} >
+								<Container>
+									<Row>
+									<Col />
+										<Col>
+											<div style={{width: '390px', height: '390px', alignContent: 'center', alignItems: 'center'}}>
+												<Card>
+													{imageDisplay}
+												</Card>
+											</div>
+										</Col>
+										<Col />
+									</Row>
+								</Container>
+							</Col>
+						</Row>
+					</Col>
+
+					<Col md={{ span: 5, offset: 1 }}>
+						<div style={{margin: '20px'}}/>
+						<Card style={{textAlign: 'center'}}>
+						<Card.Body>
+							<Card.Title style={{fontWeight:'bold'}}>What Other's Are Saying About This Shoe</Card.Title>
+							<ShoeStats percentage={"75%"} quality={"say this shoe is stiff"}/>
+							<ShoeStats percentage={"70%"} quality={"say this shoe is comfortable"}/>
+							<ShoeStats percentage={"85%"} quality={"say this shoe is lightweight"}/>
+							<ShoeStats percentage={"73%"} quality={"say this shoe is narrow"}/>
+							<ShoeStats percentage={"80%"} quality={"would buy this shoe again"}/>
+							<HorizLineBottom color='lightGray'/>
+						</Card.Body>
+						</Card>
+					</Col>
+					</Row>
 					<div style={{margin: '20px'}} />
 					<Row>
-						<Col style={{position: 'relative', left: 0, right: 0, top: 0, bottom: 0}} >
-							<Container>
-								<Row>
-								<Col />
-									<Col>
-										<div style={{width: '350px', height: '350px', alignContent: 'center', alignItems: 'center'}}>
-											<Card>
-												{imageDisplay}
-											</Card>
-										</div>
-									</Col>
-									<Col />
-								</Row>
-							</Container>
+						<Col>
+						<Card style={{height: '200px', overflowY: 'scroll'}}>
+							<Card.Body>
+								<Card.Title style={{fontWeight:'bold'}}>Comments</Card.Title>
+								<HorizLineTop color='lightGray'/>
+								<Card.Text>"One user said they really like this shoe a lot"</Card.Text>
+								<Card.Text>"this shoe is horrible don't buy it"</Card.Text>
+								<Card.Text>"this shoe is great but your website sucks"</Card.Text>
+								<Card.Text>"i am a user"</Card.Text>
+							</Card.Body>
+						</Card>
+						<div style={{ margin: '20px'}} />
 						</Col>
 					</Row>
-				</Col>
-
-				<Col md={{ span: 5, offset: 1 }}>
-					<div style={{margin: '20px'}}/>
-					<Card style={{textAlign: 'center'}}>
-					<Card.Body>
-						<Card.Title style={{fontWeight:'bold'}}>What Other's Are Saying About This Shoe</Card.Title>
-						<ShoeStats percentage={"75%"} quality={"say this shoe is stiff"}/>
-						<ShoeStats percentage={"70%"} quality={"say this shoe is comfortable"}/>
-						<ShoeStats percentage={"85%"} quality={"say this shoe is lightweight"}/>
-						<ShoeStats percentage={"73%"} quality={"say this shoe is narrow"}/>
-						<ShoeStats percentage={"80%"} quality={"would buy this shoe again"}/>
-						<HorizLineBottom color='lightGray'/>
-					</Card.Body>
-					</Card>
-				</Col>
-				</Row>
-				<div style={{margin: '20px'}} />
-				<Row>
-					<Col>
-					<Card style={{height: '200px', overflowY: 'scroll'}}>
-						<Card.Body>
-							<Card.Title style={{fontWeight:'bold'}}>Comments</Card.Title>
-							<HorizLineTop color='lightGray'/>
-							<Card.Text>"One user said they really like this shoe a lot"</Card.Text>
-							<Card.Text>"this shoe is horrible don't buy it"</Card.Text>
-							<Card.Text>"this shoe is great but your website sucks"</Card.Text>
-							<Card.Text>"i am a user"</Card.Text>
-						</Card.Body>
-					</Card>
-					<div style={{ margin: '20px'}} />
-					</Col>
-				</Row>
-			</Container>
-			</div>
-		)
+				</Container>
+				</div>
+			)
+		}
 	}
 }
 
