6bb3a7fa2f899bc686e350a799d1968ac0e0e093
Adam Klein <agklein98@gmail.com>
2020-06-03 16:23:18 -0700

    Clean 9 code smells

--- src/test/java/com/truesize/tests3/TestDirectedShoeEdge.java
+++ src/test/java/com/truesize/tests3/TestDirectedShoeEdge.java
@@ -1,6 +1,8 @@
 package com.truesize.tests3;
 
-import com.truesize.shoegraph.*;
+import com.truesize.shoegraph.AllShoeRepository;
+import com.truesize.shoegraph.DirectedShoeEdge;
+import com.truesize.shoegraph.ShoeNode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,20 +35,5 @@ public class TestDirectedShoeEdge {
                 ShoeNode.generateUniqueCode("T2c", ADIDAS, F));
         DirectedShoeEdge de = new DirectedShoeEdge(3, -2, sn);
         assert(de.toString().contains("-2"));
-    }
-
-    private void addTestGroup(){
-        //---Test Group 4: testing edge changes when add multiple shoe nodes that increase edge's multiplicity---
-        ShoeNode t1c = new ShoeNode("T1c",ADIDAS, F);
-        ShoeNode t2c = new ShoeNode("T2c",ADIDAS, F);
-        ShoeNode t3c = new ShoeNode("T3c",ADIDAS, F);
-
-        t1c.addEdge(t2c, 10.0, 9.0);
-        t1c.addEdge(t2c, 10.0, 9.5);
-
-        t2c.addEdge(t3c, 9.0, 8.5);
-        this.allShoeRepository.save(t1c);
-        this.allShoeRepository.save(t2c);
-        this.allShoeRepository.save(t3c);
     }
 }
--- src/test/java/com/truesize/tests3/TestOwnedShoe.java
+++ src/test/java/com/truesize/tests3/TestOwnedShoe.java
@@ -1,20 +1,10 @@
 package com.truesize.tests3;
 
 import com.truesize.OwnedShoe;
-import com.truesize.shoegraph.AllShoeRepository;
-import com.truesize.shoegraph.ShoeNode;
-import com.truesize.shoegraph.ShoeSizeRecommendSearcherBFS;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,7 +18,7 @@ public class TestOwnedShoe {
 
     @Test
     public void testToString() {
-        OwnedShoe testShoe = new OwnedShoe("T1a", "Adidas", 500.0, "f", "https://fakeurl.com");
+        OwnedShoe testShoe = new OwnedShoe("T1a", "Adidas", 500.0, "f", "https://fakerurl.com");
         assert(testShoe.toString().contains("500.0"));
 
     }
